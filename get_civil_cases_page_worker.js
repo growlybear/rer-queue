3c40c7500f8db9c83d35acff04e0dbbb38b9081e
Michael Allan <michaelallan@optushome.com.au>
2014-10-30 07:11:15 +1100

    Get daily list link from page html

--- get_civil_cases_page_worker.js
+++ get_civil_cases_page_worker.js
@@ -1,5 +1,6 @@
 var url = require('url');
 var needle = require('needle');
+var cheerio = require('cheerio');
 
 // Fetch the Vic Supreme Court page with the daily Civil Court list link
 var page = url.format({
@@ -8,16 +9,38 @@ var page = url.format({
     pathname: 'home/forms+fees+and+services/registry+services/list+of+civil+cases/'
 });
 
+var selector = '#publication a';
+var link, $;
+
 // NOTE don't forget to explicity exit on completion
 needle.get(page, function (error, response) {
+
+    // Something bad happened :-(
     if (error) {
-        console.log(response.body);
+        console.error(error);
         process.exit(1);
-    } else if (response.statusCode !== 200) {
-        console.log(response.statusCode);
+    }
+
+    // Imperfect response
+    else if (response.statusCode !== 200) {
+        console.error(
+            'GET request failed with response.statusCode:',
+            response.statusCode
+        );
         process.exit(1);
-    } else {
-        console.log(response.body);
+    }
+
+    // We're good, let's go
+    else {
+        $ = cheerio.load(response.body);
+        link = $(selector).attr('href');
+
+        if (!link) {
+            console.error('No Court List link found in document');
+        } else {
+            console.log(link);
+        }
+
         process.exit(0);
     }
 });
