dbb139c9d09e7ca9eddcd2328e83c4e1296b061a
Michael Allan <michaelallan@optushome.com.au>
2014-10-30 10:10:49 +1100

    Override default url and selector if parameters provided

--- get_case_list_link_worker.js
+++ get_case_list_link_worker.js
@@ -5,21 +5,37 @@ var cheerio = require('cheerio');
 // Pre-installed Iron.io helper module for accessing config and payload variables
 var worker = require('node_helper');
 
-// Just checking
-console.log("params:", worker.params);
-console.log("config:", worker.config);
-console.log("task_id:", worker.task_id);
+var DEFAULTS = {
+    // Vic Supreme Court page with the daily Civil Court list link
+    page: url.format({
+        protocol: 'http',
+        hostname: 'www.supremecourt.vic.gov.au',
+        pathname: 'home/forms+fees+and+services/registry+services/list+of+civil+cases/'
+    }),
+    selector: '#publication a'
+};
 
+var page = DEFAULTS.page;
+var selector = DEFAULTS.selector;
 
-// Fetch the Vic Supreme Court page with the daily Civil Court list link
-var page = url.format({
-    protocol: 'http',
-    hostname: 'www.supremecourt.vic.gov.au',
-    pathname: 'home/forms+fees+and+services/registry+services/list+of+civil+cases/'
-});
+// Override defaults if parameters are provided
+if (worker.params) {
+    if (worker.params.url) {
+        page = worker.params.url;
+    }
+    if (worker.params.selector) {
+        selector = worker.params.selector;
+    }
 
-var selector = '#publication a';
-
+    // If neither url nor selector are specified, you're doing it wrong ;-)
+    if (!worker.params.url && !worker.params.selector) {
+        console.error(
+            'Invalid params. Object must specify url and/or selector: ' +
+            '{ url: url, selector: selector }'
+        );
+        process.exit(1);
+    }
+}
 
 // NOTE don't forget to explicity exit on completion
 needle.get(page, function (error, response) {
@@ -35,7 +51,7 @@ needle.get(page, function (error, response) {
     // Imperfect response
     else if (response.statusCode !== 200) {
         console.error(
-            'GET request failed with response.statusCode:',
+            'GET request failed with status code:',
             response.statusCode
         );
         process.exit(1);
@@ -43,12 +59,14 @@ needle.get(page, function (error, response) {
 
     // We're good, let's go
     else {
+
         $ = cheerio.load(response.body);
         link = $(selector).attr('href');
 
         if (!link) {
             console.error('No Court List link found in document');
-        } else {
+        }
+        else {
             console.log(link);
         }
 
